b7eea171373e04c7b3715db138572dcedbd543c9
yuzf <yuzf@asiainfo.com>
2020-01-16 16:07:01 +0800

    页面上传

---
+++ web-res/app/yuzf/web/js/initPage.js
@@ -0,0 +1,8 @@
+$("#sign").click(function () {
+    $("#userNumber").show();
+    $("#passwd-repeat").show();
+});
+$("#loginOn").click(function () {
+    $("#userNumber").hide();
+    $("#passwd-repeat").hide();
+})
